ab58a62c733b1a0197b033b6002222584688fdc7
OOTANI,Kenji <ootani@virgintech.co.jp>
2015-10-19 20:41:53 +0000

    avatar image FileDialog

--- app/assets/javascripts/avatar_drag.js
+++ app/assets/javascripts/avatar_drag.js
@@ -1,13 +1,59 @@
-  /*/ File API実装チェック
+  // File API実装チェック
   if (window.File) {
-    window.alert("File APIが実装されてます。");
+    //window.alert("File APIが実装されてます。");
     // File APIが実装されている場合には、dropイベントを登録
     document.getElementById("drop").addEventListener("drop", onDrop, false);
   } else {
     window.alert("本ブラウザではFile APIが使えません");
-  }*/
+  }
+  
+  //==============================
+  //ファイル選択ダイアログ
+  //==============================
+  function fileChange(event) 
+  {
+    var file = event.target.files[0];
+    var disp = document.getElementById("disp");
+    var name = document.getElementById("name");
+    disp.innerHTML ="";
+    name.innerHTML ="";
+    
+    //FileReaderオブジェクトの生成
+    var reader = new FileReader();
 
-  //Drop領域にドロップした際のファイルのプロパティ情報読み取り処理
+    //画像ファイルかテキスト・ファイルかを判定
+    if (!file.type.match('image.*')) 
+    {
+      alert("画像ファイル以外は表示できません。");
+    }
+
+    //エラー発生時の処理
+    reader.onerror = function (evt) 
+    {
+      disp.innerHTML = "読み取り時にエラーが発生しました。";
+    }
+
+    //画像ファイルの場合の処理
+    if (file.type.match('image.*')) 
+    {
+      //ファイル読取が完了した際に呼ばれる処理
+      reader.onload = function (evt) 
+      {
+        var img = document.createElement('img');
+        img.src = evt.target.result;
+        img.width=100;
+        img.height=100;
+        disp.appendChild(img);
+        name.innerHTML = file.name;
+      }
+      // readAsDataURLメソッドでファイルの内容を取得
+      reader.readAsDataURL(file);
+    }
+  }
+  
+  //==============================
+  //ドロップ操作
+  //==============================
   function onDrop(event) 
   {
     var files = event.dataTransfer.files;
